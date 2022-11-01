a9bb8cc8a1e34b1253bb886856ff050d27d1fb56
Rojan <rojan108p@gmail.com>
2022-11-01 14:26:51 +1100

    Practice Code Wrote the code to display similar UI.

--- src/App.js
+++ src/App.js
@@ -1,67 +1,3 @@
-function ProductCategoryRow({ category }) {
-  return (
-    <tr>
-      <th colSpan="2">{category}</th>
-    </tr>
-  );
-}
-
-function ProductRow({ product }) {
-  const name = product.stocked ? (
-    product.name
-  ) : (
-    <span style={{ color: "red" }}>{product.name}</span>
-  );
-
-  return (
-    <tr>
-      <td>{name}</td>
-      <td>{product.price}</td>
-    </tr>
-  );
-}
-
-function ProductTable({ products }) {
-  const rows = [];
-  let lastCategory = null;
-
-  products.forEach((product) => {
-    if (product.category !== lastCategory) {
-      rows.push(
-        <ProductCategoryRow
-          category={product.category}
-          key={product.category}
-        />
-      );
-    }
-    rows.push(<ProductRow product={product} key={product.name} />);
-    lastCategory = product.category;
-  });
-  console.log({ rows });
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </table>
-  );
-}
-
-function SearchBar() {
-  return (
-    <form>
-      <input type="text" placeholder="Search..." />
-      <label>
-        <input type="checkbox" /> Only show products in stock
-      </label>
-    </form>
-  );
-}
-
 function FilterableProductTable({ products }) {
   return (
     <div>
@@ -71,13 +7,74 @@ function FilterableProductTable({ products }) {
   );
 }
 
+function SearchBar() {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Search..." />
+        <label>
+          <input type="checkbox" /> Only show products in stock
+        </label>
+      </form>
+    </div>
+  );
+}
+
+function ProductTable({ products }) {
+  const fruit = [];
+  const veg = [
+    <ProductCategoryRow category={"Vegetables"} key={"Vegetables"} />
+  ];
+  fruit.push(<ProductCategoryRow category={"Fruit"} key={"Fruits"} />);
+
+  products.forEach((x) => {
+    if (x.category === "Fruits") {
+      fruit.push(<ProductRow items={x} key={x.name} />);
+    } else {
+      veg.push(<ProductRow items={x} key={x.name} />);
+    }
+  });
+
+  return (
+    <div>
+      <table>
+        <th>
+          <td>Name</td>
+          <td>Price</td>
+        </th>
+        <tbody>
+          {fruit}
+          {veg}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function ProductCategoryRow({ category }) {
+  return (
+    <tr>
+      <th colSpan="2">{category}</th>
+    </tr>
+  );
+}
+
+function ProductRow({ items }) {
+  return (
+    <tr>
+      <td>{items.name}</td>
+      <td>{items.price}</td>
+    </tr>
+  );
+}
+
 const PRODUCTS = [
   { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
   { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
   { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
   { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" }
 ];
 
 export default function App() {
